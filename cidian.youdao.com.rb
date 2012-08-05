0fb842b37032e509ae37295983be2e7405110ec1
seoaqua <seoaqua@qq.com>
2012-08-05 13:48:10 +0800

    updated

--- cidian.youdao.com.rb
+++ cidian.youdao.com.rb
@@ -1,33 +1,14 @@
 #coding:UTF-8
+$project = 'youdao'
 require './common.rb'
 host = 'cidian.youdao.com'
-uri_patterns = [] <<
-    %r(^/(android|ipad|windows-phone|iphone|mac|changelog|feature|history|)\.html)
 describe host do
-    meta[:redirects] = [] << 
-    %w(/cet /cet/) <<
-    %w(/cet /cet/) <<
-    %w(/dictlib.html /) <<
-    %w(/faq2.html /faq.html) <<
-    %w(/5.0/help/deskdict5beta/faq/index.html /faq.html) <<
-    %w(/beta /beta/) <<
-    %w(/qiye /qiye/) <<
-    %w(/features /features/) <<
-    %w(/v3 /) <<
-    %w(/v2/ /) <<
-    %w(/v2 /) <<
-    %w(/4.1/ /) <<
-    %w(/4.1 /) <<
-    %w(/4.0/ /) <<
-    %w(/4.0 /) <<
-    %w(/5.0 /) <<
-    %w(/5.0/ /) <<
-    %w(?sitemap /)
+    it_behaves_like '所有主机',host
 end
 uri = 'http://cidian.youdao.com/'
 describe "桌面词典首页#{uri}" do
     meta = {}
     meta[:uri] = uri
-    meta[:title] = ''
+    meta[:title] = '英语_汉语_法语_日语_韩语_有道多语言桌面词典_字典_翻译软件'
     it_behaves_like '所有页面',meta
 end
