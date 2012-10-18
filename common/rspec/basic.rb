20282b4c3c54c300f81d58b60acc8fea9dcb7dc9
zhutingting <120223261@qq.com>
2012-10-18 11:30:05 +0800

    \t --> __

--- common/rspec/basic.rb
+++ common/rspec/basic.rb
@@ -2,14 +2,14 @@
 
 shared_examples "BASIC" do
   it "标题符合规范" do
-	  @page.title == @meta.title
-	end
-	
-	it "关键字符合规范" do
-	  @page.keywords == @meta.keywords
-	end
+    @page.title == @meta.title
+  end
+  
+  it "关键字符合规范" do
+    @page.keywords == @meta.keywords
+  end
 
-	it "描述符合规范" do
-	  @page.description == @meta.description
-	end
+  it "描述符合规范" do
+    @page.description == @meta.description
+  end
 end
--- common/rspec/check.rb
+++ common/rspec/check.rb
@@ -8,25 +8,25 @@ regexp_path = './regexp';
 
 class Meta
   attr_accessor :title, :keywords, :description
-	 
+   
   def initialize(line) # line from regexp_file
-	  items = line.split
-		@title       = items[2]
-		@keywords    = items[5]
-	  @description = items[7]
+    items = line.split
+    @title       = items[2]
+    @keywords    = items[5]
+    @description = items[7]
   end
 end
 
 class Page
   attr_accessor :title, :keywords, :description
-	 
+   
   def initialize(line)
-	  url = (line.split)[1]
-		agent = Mechanize.new
-		agent.get(url)
-		@title       = agent.page.title                                                             
-		@keywords    = agent.page.search("meta[name='keywords']")[0].attributes['content'].value    
-	  @description = agent.page.search("meta[name='description']")[0].attributes['content'].value 
+    url = (line.split)[1]
+    agent = Mechanize.new
+    agent.get(url)
+    @title       = agent.page.title                                                             
+    @keywords    = agent.page.search("meta[name='keywords']")[0].attributes['content'].value    
+    @description = agent.page.search("meta[name='description']")[0].attributes['content'].value 
   end
 end
 
