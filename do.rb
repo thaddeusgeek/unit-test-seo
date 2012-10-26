c681994f491bd98b869726779d1ddad1ebeb90fb
zhutingting <120223261@qq.com>
2012-10-26 16:43:11 +0800

    change regex(\t separated) to meta.csv(, separated)

--- do.rb
+++ do.rb
@@ -1,5 +1,6 @@
 # coding: UTF-8
 
+require 'csv'
 require 'uri'
 require 'webpage'
 require 'mechanize'
@@ -17,19 +18,27 @@ Dir.new(project).each do |domain|
   Dir.new(File.join(project,domain)).each do |host|
     next if host == '.' or host == '..'
     next if File.exists? File.join(project,domain,host,'skip') #若包含skip文件,则跳过整个文件夹不处理
-    next if !File.exists? File.join(project,domain,host,'regex') #不存在regex文件 跳过
-    regex = File.join(project,domain,host,'regex') 
-    File.open(regex).each do |line|
-      line = line.split(/\s+/)
-      next if line[4].nil? || line[6].nil?
+    next if !File.exists? File.join(project,domain,host,'meta.csv') #不存在meta.csv文件 跳过
+    meta_csv = File.join(project,domain,host,'meta.csv') 
+
+		begin
+		  CSV.open(meta_csv).each { |row| }
+		rescue CSV::MalformedCSVError
+      puts "!!!!!#{meta_csv} is malformed"
+			next
+		end
+
+    CSV.open(meta_csv).each do |row|
+			items = row
+      next if items[4].nil? || items[6].nil?
 
       meta = {}
-      meta[:uri] = line[1]
-      meta[:keywords] = line[5].split(',') # 返回一个 keywords 的数组
-      meta[:keywords] = line[5] if line[6] == '\identical' #\identical标记表示和举例的一致
-      meta[:title] = Regexp.new line[4]
-      meta[:title] = line[3] if line[4] == '\identical' #\identical标记表示和举例的一致
-      meta[:description] = line[7] if !line[7].nil?
+      meta[:uri] = items[1]
+      meta[:keywords] = items[5].split(',') # 返回一个 keywords 的数组
+      meta[:keywords] = items[5] if items[6] == '\identical' #\identical标记表示和举例的一致
+      meta[:title] = Regexp.new items[4]
+      meta[:title] = items[3] if items[4] == '\identical' #\identical标记表示和举例的一致
+      meta[:description] = items[7] if !items[7].nil?
 
       agent = Mechanize.new
       page = Webpage.new(agent.get(meta[:uri]).body)
